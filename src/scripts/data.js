f2214cc8c7d20f2ccd3230dfbee73e36e7e1d601
guilherme3110 <61066086+guilherme3110@users.noreply.github.com>
2022-11-22 15:09:01 -0300

    atividade-organizando-um-projeto-front-end

---
+++ src/scripts/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        name:'Caderno',
+        image:'./img/product.jpg',
+        price:'R$50,00',
+        off: 10
+    },
+    {
+        name:'Livro',
+        image:'./img/product.jpg',
+        price:'R$80,00',
+        off: 20
+    },{
+        name:'Apontador',
+        image:'./img/product.jpg',
+        price:'R$5,00',
+        off: 30
+    },{
+        name:'Lápis',
+        image:'./img/product.jpg',
+        price:'R$2,50',
+        off: 0
+    },{
+        name:'Tesoura',
+        image:'./img/product.jpg',
+        price:'R$6,00',
+        off: 10
+    },{
+        name:'Canetinhas',
+        image:'./img/product.jpg',
+        price:'R$15,00',
+        off: 50
+    },{
+        name:'Estojo',
+        image:'./img/product.jpg',
+        price:'R$25,00',
+        off: 0
+    },{
+        name:'Régua',
+        image:'./img/product.jpg',
+        price:'R$5,00',
+        off: 0
+    },{
+        name:'Mochila',
+        image:'./img/product.jpg',
+        price:'R$250,00',
+        off: 20
+    },{
+        name:'Giz de Cera',
+        image:'./img/product.jpg',
+        price:'R$18,00',
+        off: 80
+    },
+]
